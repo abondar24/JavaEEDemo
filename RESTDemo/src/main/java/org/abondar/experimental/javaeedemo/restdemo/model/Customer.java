1c10ec10c74e4fa5db50d56aba4dae7ba96154d3
abondar24 <abondar1992@gmail.com>
2017-12-12 22:10:16 +0100

    REST Demo base

---
+++ RESTDemo/src/main/java/org/abondar/experimental/javaeedemo/restdemo/model/Customer.java
@@ -0,0 +1,132 @@
+package org.abondar.experimental.javaeedemo.restdemo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.util.Date;
+
+@Entity
+@Table(name="customer")
+@Access(AccessType.FIELD)
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+    @NotNull
+    @Column(name="first_name",nullable = false,length=50)
+    private String firstName;
+
+    @Column(name="last_name",nullable = false,length=50)
+    private String lastName;
+
+    private String email;
+
+    @Column(name="phone_number",length = 15)
+    private String phoneNumber;
+
+    @Past
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateOfBirth;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+
+    @Transient
+    private Integer age;
+
+    public Customer(){}
+
+    public Customer(String firstName, String lastName, String email, String phoneNumber,Date dateOfBirth, Date creationDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.creationDate = creationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //length of 15 is overwritten by 50 because of entity annotation
+    @Access(AccessType.PROPERTY)
+    @Column(name="phone_number",length = 50)
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", creationDate=" + creationDate +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ RESTDemo/src/main/java/org/abondar/experimental/javaeedemo/restdemo/model/Item.java
@@ -0,0 +1,86 @@
+package org.abondar.experimental.javaeedemo.restdemo.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Item implements Serializable {
+
+    @Id
+    @GeneratedValue
+    protected Long id;
+
+    @NotNull
+    @Column(name="title",nullable = false,updatable = false)
+    protected String title;
+
+    protected Float price;
+
+    @Size(min = 10,max=2000)
+    @Column(length = 2000)
+    protected String description;
+
+    protected String currency;
+
+    public Item(){}
+
+    public Item(String title, Float price, String description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
